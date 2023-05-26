68ee637326f865ede4ad59b0aed250fea54ee06b
marijastanojcic <marijast92@gmail.com>
2023-05-26 00:34:33 -0700

    date-picker component not available in vuetify 3;

--- my-hotel-app/src/views/Home.vue
+++ my-hotel-app/src/views/Home.vue
@@ -2,13 +2,11 @@
 
   <BookingSelection/>
   <RoomsAvailability></RoomsAvailability>
-
   <ContactInformation />
 </template>
 
 <script setup>
   import BookingSelection from "@/components/BookingSelection.vue";
   import RoomsAvailability from "@/components/RoomsAvailability.vue";
-  import ContactInformation from '@/components/ContactInformation.vue'
-  //import DatePicker from '@/components/DatePicker.vue';
+  import ContactInformation from '@/components/ContactInformation.vue';
 </script>

937089b41f9eba2f0e0320ddb1cca609b8b7f7c5
Marija Stanojcic <marijast92@gmail.com>
2023-06-04 21:56:52 -0700

    Personal and Billing form now save the previously input information;

--- my-hotel-app/src/components/Guest.vue
+++ my-hotel-app/src/components/Guest.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-container class="section-container">
+  <v-container class="section-container" >
     <v-row>
       <v-col cols="6"><h1 class="section-header"> Guest </h1></v-col>
 
     </v-row>
 
-    <v-row>
-      <v-col cols="3" >
+    <v-row style="display: flex; ">
+
         <v-list v-if="showMsg===''"
           rounded
-          style="background-color: transparent; margin: 14em 4em 4em;"
+          class="guest-list"
         >
 
             <v-list-item >
@@ -44,78 +44,96 @@
 
 
         </v-list>
-      </v-col>
-      <v-col cols="6">
-        <PersonalInformation
-          v-if="showPersonalCard"
-          @getPersonalInfo="getPersonalInformation"
-        >
 
-        </PersonalInformation>
-        <BillingInofrmation
-          v-if="showBillingCard"
-          @getBillingInfo="getBillingInformation"
-        >
-        </BillingInofrmation>
-
-        <v-col>
-          <v-card
-            v-if="showReviewCard"
-            elevation="0"
-            rounded
-            class="info-card"
+        <div style="flex: 2;">
+          <PersonalInformation
+            v-if="showPersonalCard"
+            @getPersonalInfo="getPersonalInformation"
+            :guest="guest"
           >
-            <v-card-title class="info-title"> Review </v-card-title>
+
+          </PersonalInformation>
+          <BillingInofrmation
+            v-if="showBillingCard"
+            @getBillingInfo="getBillingInformation"
+            :card="card"
+          >
+          </BillingInofrmation>
+
+          <v-col>
             <v-card
+              v-if="showReviewCard"
               elevation="0"
               rounded
-              style="margin: 2em;"
+              class="info-card"
             >
-              <v-card-text style="font-size: 18px;">
-                <span style="color: #5CA277"> Room: </span> {{this.room}}
-              </v-card-text>
-              <v-card-text style="font-size: 18px">
-                <span style="color: #5CA277"> Number of guests: </span> {{this.nGuests}}
-              </v-card-text>
-              <v-card-text style="font-size: 18px">
-                <span style="color: #5CA277"> Check-in Date: </span> {{this.checkInDate}}
-              </v-card-text>
-              <v-card-text style="font-size: 18px">
-                <span style="color: #5CA277"> Check-out Date: </span> {{this.checkOutDate}}
-              </v-card-text>
-              <v-card-text style="font-size: 18px">
-                <span style="color: #5CA277"> Price per night: </span> {{this.pricePerNight}}
-              </v-card-text>
-              <v-card-text v-if="guestRequests" style="font-size: 18px">
-                <span style="color: #5CA277"> Additional requests: </span> {{this.additionalOptions}}
-              </v-card-text>
-            </v-card>
-            <v-card-title style="margin: 2em; font-size: 22px; color:#5CA277; font-weight: bold;">
-              Total Price:  {{this.pricePerNight}} * 5
-            </v-card-title>
-            <v-card-item style="display: flex; justify-content: center;">
-              <v-checkbox
-                v-model="accept"
-                label="I accept"
-                color="#5CA277"
-                hide-details
+              <v-card-title class="info-title"> Review </v-card-title>
+              <v-card
+                elevation="0"
+                rounded
+                style="margin: 2em;"
               >
-              </v-checkbox>
+                <v-card-text style="font-size: 18px">
+                  <span style="color: #5CA277"> Number of guests: </span> {{this.nGuests}}
+                </v-card-text>
 
-            </v-card-item>
-            <v-card-item style="display: flex; justify-content: center;">
-              <v-btn class="section-btn" :disabled="!accept" @click="payment">
-                PAY
-              </v-btn>
-            </v-card-item>
-          </v-card>
+                <v-card-text style="font-size: 18px">
+                    <span style="color: #5CA277"> Check-in Date: </span> {{ this.checkInDate }}
+                  </v-card-text>
+                  <v-card-text style="font-size: 18px">
+                    <span style="color: #5CA277"> Check-out Date: </span> {{this.checkOutDate}}
+                  </v-card-text>
+                  <v-card-text style="font-size: 18px">
+                    <span style="color: #5CA277"> Total nights: </span> {{this.nNights}}
+                  </v-card-text>
 
-          <span v-if="showMsg!==''" style="color:#5CA277;">{{this.showMsg}}</span>
+                <v-card-text v-if="guestRequests" style="font-size: 18px">
+                    <span style="color: #5CA277"> Additional requests: </span> {{this.additionalOptions}}
+                </v-card-text>
+
+                <v-card v-for="(room, roomId) in rooms" :key="roomId">
+                  <v-card-text style="font-size: 18px;">
+                    <span style="color: #5CA277"> Room Type: </span> {{room.name}}
+                  </v-card-text>
+
+                  <v-card-text style="font-size: 18px;">
+                    <span style="color: #5CA277"> Number of rooms: </span> {{room.numRooms}}
+                  </v-card-text>
+
+                  <v-card-text style="font-size: 18px">
+                    <span style="color: #5CA277"> Price per room per night: </span> {{room.roomPrice}}
+                  </v-card-text>
 
 
-        </v-col>
+                </v-card>
 
-      </v-col>
+              </v-card>
+              <v-card-title style="margin: 2em; font-size: 22px; color:#5CA277; font-weight: bold;">
+                Total Price:  ${{this.totalStayPrice}}
+              </v-card-title>
+              <v-card-item style="display: flex; justify-content: center;">
+                <v-checkbox
+                  v-model="accept"
+                  label="I approve"
+                  color="#5CA277"
+                  hide-details
+                >
+                </v-checkbox>
+
+              </v-card-item>
+              <v-card-item style="display: flex; justify-content: center;">
+                <v-btn class="section-btn" :disabled="!accept" @click="payment">
+                  PAY
+                </v-btn>
+              </v-card-item>
+            </v-card>
+
+            <span v-if="showMsg!==''" style="color:#5CA277;">{{this.showMsg}}</span>
+
+
+          </v-col>
+
+        </div>
 
 
     </v-row>
@@ -128,7 +146,8 @@
 <script>
 
   import PersonalInformation from "@/components/PersonalInformation.vue";
-  import BillingInofrmation from "@/components/BillingInofrmation.vue";
+  import BillingInofrmation     from "@/components/BillingInofrmation.vue";
+  import {mapGetters} from "vuex";
 
 
 
@@ -153,13 +172,25 @@ export default {
     nGuests: '',
     checkInDate: '',
     checkOutDate: '',
+    nNights: null,
     pricePerNight: '',
+    totalStayPrice: null,
     additionalOptions: '',
     accept: false,
     payed: false,
     showMsg: '',
+    rooms:{},
   }),
 
+  computed:{
+    ...mapGetters([
+      'getCheckInDate',
+      'getCheckOutDate',
+      'getTotalGuests',
+      'getChoosenRooms'
+    ]),
+
+  },
   methods: {
     getPersonalInformation(guest){
       this.guest = guest;
@@ -174,6 +205,16 @@ export default {
       this.showBillingCard = false;
       this.showReviewCard = true;
       console.log('showBillingCard', this.showBillingCard)
+      console.log('getTotalGuests', this.getTotalGuests)
+      console.log('getCheckInDate', this.getCheckInDate)
+      console.log('getCheckOutDate', this.getCheckOutDate)
+      console.log('getChoosenRooms', JSON.parse(JSON.stringify(this.getChoosenRooms)))
+      this.nGuests = this.getTotalGuests
+      this.checkInDate = this.getCheckInDate
+      this.checkOutDate = this.getCheckOutDate
+      this.nNights = this.numberOfNights(this.checkInDate, this.checkOutDate)
+      this.rooms = JSON.parse(JSON.stringify(this.getChoosenRooms))
+      this.totalStayPrice = this.calculateTotalPrice()
     },
     showPersonal(){
       this.showBillingCard = false;
@@ -186,6 +227,14 @@ export default {
       this.showBillingCard = true;
     },
     showReview(){
+      console.log('getCheckInDate', this.getCheckInDate)
+      console.log('getCheckOutDate', this.getCheckOutDate)
+      this.checkInDate=this.getCheckInDate
+      this.checkOutDate=this.getCheckOutDate
+      console.log('checkin date', this.checkInDate)
+      console.log('checkout date', this.checkOutDate)
+
+
       this.showPersonalCard = false;
       this.showBillingCard = false;
       this.showReviewCard = true;
@@ -199,6 +248,33 @@ export default {
       this.showReviewCard = false;
 
       this.showMsg = 'You successfully booked your stay with us. You will get an email confirmation shortly.'
+      this.$emit('payed')
+    },
+    numberOfNights(startDate, endDate){
+      const timeDiff = new Date(endDate).getTime() - new Date(startDate).getTime()
+
+      return Math.floor(timeDiff/(24 * 60 * 60 * 1000))
+    },
+    calculateTotalPrice(){
+
+      let totalPrice = 0
+      for (let k in this.rooms){
+        console.log('key ID rom', k)
+        let nRooms = parseInt(this.rooms[k]['numRooms'])
+        console.log('one room', this.rooms[k])
+        console.log('one room numRooms', this.rooms[k]['numRooms'])
+        console.log('one room roomPrice', this.rooms[k]['roomPrice'])
+        let price = parseInt(this.rooms[k]['roomPrice'].replace(/\D/g, ''))
+
+        console.log('nRooms', nRooms)
+        console.log('price', price)
+        totalPrice += nRooms * price
+        console.log('totalPrice', totalPrice)
+      }
+      console.log(' total Nights ', this.nNights)
+      console.log(' final totalPrice', totalPrice)
+
+      return totalPrice * this.nNights
     },
   }
 }
@@ -215,6 +291,12 @@ export default {
   color: #5CA277;
   margin-bottom: 1em;
   margin-top: 1em;
+}
+.guest-list{
+  background-color: transparent;
+  flex: 1;
+  align-self: center;
+  margin-top: -2em;
 }
 
 .guest-list-btn{
